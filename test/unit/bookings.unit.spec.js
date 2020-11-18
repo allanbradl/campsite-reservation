e442608d78045ca93d517b59d0a54451ff4600ff
David Hunter <david@flowhub.com>
2020-11-17 19:58:26 -0700

    add test for isSearchBookable

--- test/unit/bookings.unit.spec.js
+++ test/unit/bookings.unit.spec.js
@@ -1,6 +1,7 @@
 const {
   getBookableCampsights,
   getGapSize,
+  isSearchBookable,
 } = require("../../src/controllers/bookings");
 
 describe("Bookings - Unit", () => {
@@ -26,6 +27,35 @@ describe("Bookings - Unit", () => {
     it("should return gap of one if end greater than start", () => {
       const gapSize = getGapSize(new Date(2020, 1, 20), new Date(2020, 1, 22));
       expect(gapSize).toBe(1);
+    });
+  });
+
+  describe("isSearchBookable", () => {
+    it("should return false based on start of booking", () => {
+      const isBookable = isSearchBookable(
+        new Date(2020, 1, 20),
+        new Date(2020, 1, 22),
+        { endDate: new Date(2020, 1, 18) }
+      );
+      expect(isBookable).toBe(false);
+    });
+
+    it("should return false based on end of booking", () => {
+      const isBookable = isSearchBookable(
+        new Date(2020, 1, 20),
+        new Date(2020, 1, 22),
+        { startDate: new Date(2020, 1, 24) }
+      );
+      expect(isBookable).toBe(false);
+    });
+
+    it("should return true", () => {
+      const isBookable = isSearchBookable(
+        new Date(2020, 1, 20),
+        new Date(2020, 1, 22),
+        { startDate: new Date(2020, 1, 30), endDate: new Date(2020, 1, 31) }
+      );
+      expect(isBookable).toBe(true);
     });
   });
 });
